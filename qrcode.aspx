3c84598f155058154124550d5858edc5b6691a74
hellopritam <pritghosh96@gmail.com>
2020-07-11 19:10:25 +0530

    first commit

---
+++ qrcode.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="qrcode.aspx.vb" Inherits="qrcode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Button" />
+        <br />
+    
+    </div>
+        <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ qrcode.aspx.vb
@@ -0,0 +1,43 @@
+﻿Imports QRCoder
+Imports System.Data.SqlClient
+Imports System.IO
+Imports System.Drawing
+Imports QRCoder.QRCodeGenerator.QRCode
+
+Partial Class qrcode
+    Inherits System.Web.UI.Page
+    
+
+
+
+
+
+
+
+
+
+
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim code As String
+        Dim qrGenerator As QRCodeGenerator
+        Dim qrcode As QRCodeGenerator.QRCode
+        Dim imgBarCode As System.Web.UI.WebControls.Image
+        Dim bitMap As Bitmap
+        Dim ms As MemoryStream
+        Dim byteImage As Byte()
+        code = TextBox1.Text
+        qrGenerator = New QRCodeGenerator()
+        qrcode = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q)
+        imgBarCode = New System.Web.UI.WebControls.Image()
+        imgBarCode.Height = 250
+        imgBarCode.Width = 250
+        bitMap = qrcode.GetGraphic(20)
+        ms = New MemoryStream()
+        bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png)
+        byteImage = ms.ToArray()
+        imgBarCode.ImageUrl = "data:image/png;base64," & Convert.ToBase64String(byteImage)
+        PlaceHolder1.Controls.Add(imgBarCode)
+        Label1.Visible = True
+    End Sub
+End Class
